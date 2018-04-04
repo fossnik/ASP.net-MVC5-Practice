0f7487a42f2f15868055a212b3c868031200aebf
Zack <zackhartmann@gmail.com>
2018-04-04 13:28:59 -0400

    Implemented Attribute Routing

--- Vidley/Controllers/MoviesController.cs
+++ Vidley/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Web;
 using System.Web.Mvc;
 using Vidley.Models;
@@ -16,22 +17,28 @@ namespace Vidley.Controllers
             return View(movie);
         }
 
-        public ActionResult Edit(int id)
-        {
-            return Content("id=" + id);
-        }
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content("id=" + id);
+        //}
 
-        // movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            // record parameters from URL
-            if (pageIndex.HasValue)
-                pageIndex = 1;
-            if (String.IsNullOrEmpty(sortBy))
-                sortBy = "Name";
+        //// movies
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        //{
+        //    // record parameters from URL
+        //    if (pageIndex.HasValue)
+        //        pageIndex = 1;
+        //    if (String.IsNullOrEmpty(sortBy))
+        //        sortBy = "Name";
 
-            // return simple content (the URL parameters)
-            return Content("pageIndex=" + pageIndex + "&sortBy=" + sortBy);
+        //    // return simple content (the URL parameters)
+        //    return Content("pageIndex=" + pageIndex + "&sortBy=" + sortBy);
+        //}
+
+        [Route("MoviesController/released/{year}/{month:regex(\\d{4}):range(1,12)")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
