02fba3a07c3c6bcae2e1f1aaca67e6fe717e6e06
Zack <zackhartmann@gmail.com>
2018-04-07 12:54:46 -0400

    Added New Practice Project

---
+++ Practice-1/Models/Coin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practice_1.Models
+{
+    public class Coin
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Change { get; set; }
+        public double PChange { get; set; }
+        public double MarketCap { get; set; }
+        public double Volume { get; set; }
+        public double Volume24H { get; set; }
+        public double TotalVolume24H { get; set; }
+        public double CirculatingSupply { get; set; }
+    }
+}
