bcc70e3e4a7479e4a8d5c677cf513746509a79c2
Zack <zackhartmann@gmail.com>
2018-04-09 11:59:33 -0400

    Initial - Entity Practice

---
+++ EntityPractice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
