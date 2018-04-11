253c0b0028dfe3d85a7d07bcae61c79ef36264a3
Zack <zackhartmann@gmail.com>
2018-04-11 11:18:16 -0400

    Implemented Membership Type

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -5,5 +5,9 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
+
+        // navigation property allows us to navigate from one type to another
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
